d8ba0212c9e4ba1e294d0088190c3f276f319550
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-03-17 17:33:06 -0300

    Adicionar arquivos de projeto.

---
+++ Almoxarifado/Conexao/InternetService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Almoxarifado.Conexao
+{
+    public class InternetService
+    {
+        public bool VerificarConexaoInternet()
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var reply = ping.Send("www.google.com");
+                    return reply != null && reply.Status == IPStatus.Success;
+                }
+            }
+            catch (PingException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Almoxarifado/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Almoxarifado.Conexao;
+using Almoxarifado.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Almoxarifado.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly InternetService _internetService;
+
+        public HomeController(InternetService internetService)
+        {
+            _internetService = internetService;
+        }
+
+        public IActionResult Index()
+        {
+            if (_internetService.VerificarConexaoInternet())
+            {
+                return View();
+            }
+            else
+            {
+                return BadRequest("Não foi possível estabelecer conexão com a internet! Favor verificar a conexão");
+            }
+        }
+/*
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
