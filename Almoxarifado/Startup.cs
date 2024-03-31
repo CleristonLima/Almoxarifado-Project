73ba9109d5f3b5919f6af4db91123f07b250a3e6
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-03-31 19:52:26 -0300

    Adicionando os botões de ferramentas e Veiculos.

--- Almoxarifado/Startup.cs
+++ Almoxarifado/Startup.cs
@@ -3,6 +3,7 @@ using Almoxarifado.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,15 +43,21 @@ namespace Almoxarifado
 
             // Outros serviços
             services.AddControllersWithViews();
+
+           /* services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();*/
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-       /* public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        } */
+        /* public void ConfigureServices(IServiceCollection services)
+         {
+             services.AddControllersWithViews();
+         } */
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
